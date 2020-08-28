a43c29055f32f121e824402cee41d6e2302b5644
Joe Kelly <kevjoekelly@gmail.com>
2020-08-27 22:59:00 -0500

    changed view directory backslash

--- src/controllers/BaseController.php
+++ src/controllers/BaseController.php
@@ -6,6 +6,6 @@ class BaseController //extends Database
 	public static function createView($view)
 	{
 		//$info = static::getDetails();
-		require_once(APP_ROOT . '\views\\' . $view . '.php');
+		require_once(APP_ROOT . '/views/' . $view . '.php');
 	}
 }
